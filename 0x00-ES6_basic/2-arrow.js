85775252ea2c17f99874228fd39aa75dfb49b639
SoSMrRaziel <lbt_alh@hotmail.fr>
2024-05-27 22:01:05 +0100

    task2

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
+
+  /* Arrow function does not have its own 'this' context,
+    so 'this' refers to the enclosing context */
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
