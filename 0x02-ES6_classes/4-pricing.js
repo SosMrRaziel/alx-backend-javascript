320ccd9287c88aa79c47c8c9b65dfcd6daae9231
SoSMrRaziel <lbt_alh@hotmail.fr>
2024-05-30 20:54:23 +0100

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,29 @@
+const Currency = require('./3-currency');
+
+class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+  set amount(amount) {
+    this._amount = amount;
+  }
+  get currency() {
+    return this._currency;
+  }
+  set currency(currency) {
+    this._currency = currency;
+  }
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
