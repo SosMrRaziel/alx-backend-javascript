0537f23dc6b98ede7fc7f5dfa21070e6dfb4f469
SoSMrRaziel <lbt_alh@hotmail.fr>
2024-05-30 19:44:58 +0100

    A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._verifyType(name, 'string');
+    this._verifyType(length, 'number');
+    this._verifyType(students, 'object');
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  _verifyType(attr, type) {
+    if (typeof attr !== type) {
+      throw TypeError(`${attr} must be a ${type}`);
+    }
+  }
+  get name() {
+    return this._name;
+  }
+  set name(name) {
+    this._name = name;
+  }
+  get length() {
+    return this._length;
+  }
+  set length(length) {
+    this._length = length;
+  }
+  get students() {
+    return this._students;
+  }
+  set students(students) {
+    this._students = students;
+  }
+}
