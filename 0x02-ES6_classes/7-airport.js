4dfbfc1df059e1973d20c399b691d8e0774fa7dc
SoSMrRaziel <lbt_alh@hotmail.fr>
2024-05-30 22:22:31 +0100

    airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,21 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+  get name() {
+    return this._name;
+  }
+  get code() {
+    return this._code;
+  }
+  set name(name) {
+    this._name = name;
+  }
+  set code(code) {
+    this._code = code;
+  }
+  getInfo() {
+    return `${this._name} (${this._code})`;
+  }
+}
