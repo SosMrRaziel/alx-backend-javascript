b467a89344aa32b5911510160ed112614bde5d96
SoSMrRaziel <lbt_alh@hotmail.fr>
2024-06-03 22:14:16 +0100

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,10 @@
-/* eslint-disable */
 function cleanSet(set, start_string) {
-  if (
-    start_string === '' ||
-    typeof start_string !== 'string' ||
-    start_string.length === 0
-  ) {
+  if (start_string === '' || typeof start_string !== 'string') {
     return '';
   }
   return [...set]
     .filter((str) => str.startsWith(start_string))
-    .map((str) => str.replace(start_string, ''))
+    .map((str) => str.slice(start_string.length))
     .join('-');
 }
 
