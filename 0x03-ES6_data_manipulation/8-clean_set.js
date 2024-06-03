9f12c368dea3be31f81ce2f6272e32e98b2a3211
SoSMrRaziel <lbt_alh@hotmail.fr>
2024-06-03 22:15:45 +0100

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,9 @@
 function cleanSet(set, start_string) {
-  if (start_string === '' || typeof start_string !== 'string') {
+  if (
+    start_string === '' ||
+    typeof start_string !== 'string' ||
+    start_string.length === 0
+  ) {
     return '';
   }
   return [...set]
