c28fd2bc589917ab4ef9844debeec0078abad60c
sosmrraziel <lbt_alh@hotmail.fr>
2024-07-23 18:19:47 +0100

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,38 +1,28 @@
 // 2-read_file.js
+
 const fs = require('fs');
 
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const lines = data.split('\n').filter((line) => line.trim() !== ''); // Remove empty lines
+    const students = lines.map((line) => line.split(','));
+    const csStudents = students.filter((student) => student[3] === 'CS');
+    const sweStudents = students.filter((student) => student[3] === 'SWE');
 
-    const students = {
-      CS: [],
-      SWE: [],
-    };
-
-    lines.forEach((line) => {
-      const [firstname, field] = line.split(',');
-      if (field === 'CS') {
-        students.CS.push(firstname);
-      } else if (field === 'SWE') {
-        students.SWE.push(firstname);
-      }
-    });
-
-    console.log(`Number of students: ${lines.length}`);
+    console.log(`Number of students: ${students.length}`);
     console.log(
-      `Number of students in CS: ${
-        students.CS.length
-      }. List: ${students.CS.join(', ')}`
+      `Number of students in CS: ${csStudents.length}. List: ${csStudents
+        .map((s) => s[0])
+        .join(', ')}`
     );
     console.log(
-      `Number of students in SWE: ${
-        students.SWE.length
-      }. List: ${students.SWE.join(', ')}`
+      `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents
+        .map((s) => s[0])
+        .join(', ')}`
     );
   } catch (error) {
-    throw new Error('Cannot load the database');
+    console.error(`Cannot load the database: ${error.message}`);
   }
 }
 
