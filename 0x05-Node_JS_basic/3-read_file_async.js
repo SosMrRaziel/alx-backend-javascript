9f6db452ab4b54c625cb9c179e91989744293e75
sosmrraziel <lbt_alh@hotmail.fr>
2024-07-23 21:02:38 +0100

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,29 +1,35 @@
 // 3-read_file_async.js
 
-const fs = require('fs').promises;
+const fs = require('fs');
 
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== ''); // Remove empty lines
-    const students = lines.map((line) => line.split(','));
-    const csStudents = students.filter((student) => student[3] === 'CS');
-    const sweStudents = students.filter((student) => student[3] === 'SWE');
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n').filter((line) => line.trim() !== ''); // Remove empty lines
+        const students = lines.map((line) => line.split(','));
 
-    console.log(`Number of students: ${students.length}`);
-    console.log(
-      `Number of students in CS: ${csStudents.length}. List: ${csStudents
-        .map((s) => s[0])
-        .join(', ')}`
-    );
-    console.log(
-      `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents
-        .map((s) => s[0])
-        .join(', ')}`
-    );
-  } catch (error) {
-    throw Error('Cannot load the database');
-  }
+        const csStudents = students.filter((student) => student[3] === 'CS');
+        const sweStudents = students.filter((student) => student[3] === 'SWE');
+
+        console.log(`Number of students: ${students.length - 1}`);
+        console.log(
+          `Number of students in CS: ${csStudents.length}. List: ${csStudents
+            .map((s) => s[0])
+            .join(', ')}`
+        );
+        console.log(
+          `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents
+            .map((s) => s[0])
+            .join(', ')}`
+        );
+
+        resolve();
+      }
+    });
+  });
 }
 
 module.exports = countStudents;
