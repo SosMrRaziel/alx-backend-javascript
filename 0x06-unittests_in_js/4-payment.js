afb669c9c24653e835a06cc316d92634644954b1
sosmrraziel <lbt_alh@hotmail.fr>
2024-07-25 21:52:54 +0100

    Stubs

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,16 +1,9 @@
 // 4-payment.js
 /*eslint-disable*/
-const Utils = require('./utils');
+const utils = require('./utils');
 
-/**
- * Sends a payment request to the API.
- * @param {number} totalAmount - The total amount.
- * @param {number} totalShipping - The total shipping cost.
- */
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const sumResult = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${sumResult}`);
-}
-
-// Example usage:
-sendPaymentRequestToApi(100, 20);
+module.exports = function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const total = utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${total}`);
+  return total;
+};
