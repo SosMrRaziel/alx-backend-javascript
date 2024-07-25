4bbde53e66b4b4004eee59633ce999ea3acc823b
sosmrraziel <lbt_alh@hotmail.fr>
2024-07-25 22:19:56 +0100

    Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,25 +1,35 @@
 // 10-api/api.js
 /*eslint-disable*/
 const express = require('express');
+
 const app = express();
 const port = 7865;
 
-// Endpoint: GET /available_payments
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.end('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.end(`Payment methods for cart ${req.params.id}`);
+});
+
 app.get('/available_payments', (req, res) => {
-  res.json({
+  const obj = {
     payment_methods: {
       credit_cards: true,
       paypal: false,
     },
-  });
+  };
+  res.json(obj);
 });
 
-// Endpoint: POST /login
 app.post('/login', (req, res) => {
-  const { userName } = req.body;
-  res.send(`Welcome ${userName}`);
+  const username = req.body.userName;
+  res.end(`Welcome ${username}`);
 });
 
 app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
+  console.log('API available on localhost port 7865');
 });
