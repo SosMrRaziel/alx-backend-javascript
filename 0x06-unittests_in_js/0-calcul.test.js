0454ad9cd330f3ba2b4851c724dae084df0ed5ba
sosmrraziel <lbt_alh@hotmail.fr>
2024-07-25 21:40:03 +0100

    Basic

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,44 +1,22 @@
 // 0-calcul.test.js
 /*eslint-disable*/
 const assert = require('assert');
-const mocha = require('mocha');
-
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-  it('should return sum of integers', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, -1), 0);
-    assert.strictEqual(calculateNumber(1, -3), -2);
-  });
+// Test case 1: Whole numbers
+const result1 = calculateNumber(1, 3);
+assert.strictEqual(result1, 4);
 
-  it('should round floats', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(0.1, 0), 0);
-    assert.strictEqual(calculateNumber(1.4, -4.5), -3);
-  });
+// Test case 2: Decimal input (rounding up)
+const result2 = calculateNumber(1, 3.7);
+assert.strictEqual(result2, 5);
 
-  it('should return the rounded number if only one is provided', () => {
-    assert.strictEqual(calculateNumber(2), 2);
-    assert.strictEqual(calculateNumber(2.7), 3);
-  });
+// Test case 3: Decimal input (both numbers rounded)
+const result3 = calculateNumber(1.2, 3.7);
+assert.strictEqual(result3, 5);
 
-  it('should cast non-numbers into numbers', () => {
-    assert.strictEqual(calculateNumber(true, '3'), 4);
-    assert.strictEqual(calculateNumber(1, '3.7'), 5);
-    assert.strictEqual(calculateNumber('1.2', 3.7), 5);
-  });
+// Test case 4: Decimal input (both numbers rounded up)
+const result4 = calculateNumber(1.5, 3.7);
+assert.strictEqual(result4, 6);
 
-  it('should throw typeerror if either param cannot be coerced to a number', () => {
-    assert.throws(() => calculateNumber('hello'), {
-      name: 'TypeError',
-      message: 'Parameters must be numbers',
-    });
-    assert.throws(() => calculateNumber(1.2, 'dog'), {
-      name: 'TypeError',
-      message: 'Parameters must be numbers',
-    });
-  });
-});
+console.log('All tests passed!');
